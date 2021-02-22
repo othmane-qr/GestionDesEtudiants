bc84be403aff95f681ef43987fa920faedc6502c
othmane-qr <othmane.qorqach@gmail.com>
2021-02-22 15:04:24 +0100

    authentification

---
+++ GestionDesEtudiants/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using GestionDesEtudiants.Areas.Identity.Data;
+using GestionDesEtudiants.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(GestionDesEtudiants.Areas.Identity.IdentityHostingStartup))]
+namespace GestionDesEtudiants.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<UserContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("SqlCon")));
+
+                services.AddDefaultIdentity<User>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<UserContext>();
+            });
+        }
+    }
+}
